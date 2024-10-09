5997ee77640d707339c381572b9509d48d639241
codingLogan <reachCodingLogan@gmail.com>
2024-10-09 07:35:14 -0600

    Start adding PayPal

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -72,16 +72,34 @@ const getOrderById = asyncHandler(async (req, res) => {
 
 /**
  * @description Update order to paid
- * @route Get /api/orders/:id/pay
+ * @route PUT /api/orders/:id/pay
  * @access Private
  */
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-  res.send("update order to paid");
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    const updatedOrder = await order.save();
+
+    res.status(200).json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("order not found");
+  }
 });
 
 /**
  * @description Update order to delivered
- * @route Get /api/orders/:id/deliver
+ * @route PUT /api/orders/:id/deliver
  * @access Private/Admin
  */
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
@@ -90,7 +108,7 @@ const updateOrderToDelivered = asyncHandler(async (req, res) => {
 
 /**
  * @description Get all orders
- * @route Get /api/orders
+ * @route GET /api/orders
  * @access Private/Admin
  */
 const getOrders = asyncHandler(async (req, res) => {
